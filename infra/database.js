9141bb5f4aa92f7ccb896297e60fd6131cb62e0f
Bruno Fernandes <bruno2025hf@gmail.com>
2026-05-14 15:15:59 +0000

    handles database ssl connection

--- infra/database.js
+++ infra/database.js
@@ -7,6 +7,9 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   });
 
   console.log("Credenciais do Postgres:", {
@@ -21,7 +24,7 @@ async function query(queryObject) {
     await client.connect();
     const result = await client.query(queryObject);
     return result;
-  } catch {
+  } catch (error) {
     console.error(error);
     throw error;
   } finally {
--- test/integration/api/v1/status/get.test.js
+++ test/integration/api/v1/status/get.test.js
@@ -7,7 +7,9 @@ test("GET to /api/v1/status should return 200", async () => {
   const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parseUpdatedAt);
 
-  expect(responseBody.dependencies.database.version).toEqual("16.0");
-  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.version).toContain("16");
+  expect(typeof responseBody.dependencies.database.max_connections).toBe(
+    "number",
+  );
   expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
